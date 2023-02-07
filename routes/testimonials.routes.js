ec0e8775c02f24186d0f55f399ae6dae13c72fde
Ksena <ksena@ip-192-168-1-7.ec2.internal>
2023-02-07 17:20:24 +0200

    created testimonials page

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,22 +1,50 @@
 const { Router } = require('express') 
 const router = Router()
 const testimonialsModel = require('../models/testimonials')
+const lastItem = require('../utils/lastItem')
+const pagination = require('../utils/pagination')
 
 router.get(
-  '/',
+  '/main-testimonials',
   async (req, res) => {
     try {
-      let findTestimonials = await testimonialsModel.find()
-      
-      let testimonials = await findTestimonials.map(item => ({
+      const { dataModel } = await lastItem(testimonialsModel, 6)
+
+      let testimonials = await dataModel.map(item => ({
         id: item._id,
-        date: item.date,
         name: item.name,
         text: item.text,
         stars: item.stars
       }))
 
       res.json(testimonials)
+    } catch (e) {
+      res.status(500).json({ message: "its Error, try again!" })
+    }
+  }
+)
+
+router.get(
+  '/testimonials',
+  async (req, res) => {
+    try {
+      const { page, limit } = req.query
+
+      const { dataModel, totalPages, currentPage } = await pagination(testimonialsModel, page, limit)
+
+      let pageData = await dataModel.map(item => ({
+        id: item._id,
+        date: item.date,
+        name: item.name,
+        text: item.text,
+        stars: item.stars
+      }))
+      
+      res.json({
+        pageData,
+        totalPages,
+        currentPage,
+      })
     } catch (e) {
       res.status(500).json({ message: "its Error, try again!" })
     }
