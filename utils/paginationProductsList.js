395ae4fc0bb6ca9ef6ba4de81c0791b730e6c1a1
Ksenaaa <103521446+Ksenaaa@users.noreply.github.com>
2024-03-19 11:09:29 +0100

    new change (#9)
    * new change
    
    * new change ..
    
    ---------
    
    Co-authored-by: Ksena <ksena@ip-192-168-1-7.ec2.internal>

--- utils/paginationProductsList.js
+++ utils/paginationProductsList.js
@@ -1,69 +1,72 @@
-const createFilterData = require("./createFilterData")
-const searchProductsByElementFilter = require("./searchProductsByElementFilter")
-const sortProducts = require("./sortProducts")
+const createFilterData = require("./createFilterData");
+const searchProductsByElementFilter = require("./searchProductsByElementFilter");
+const sortProducts = require("./sortProducts");
 
-const paginationProductsList = (async(args) => {
-  const { Products, Colors, category, page, limit, filter, sort } = args
+const paginationProductsList = async (args) => {
+  const { Products, Colors, category, page, limit, filter, sort } = args;
 
-  let productsData
-  let colorsData = await Colors.find({})
-  let filterData
-  let parseFilter
-  let findElements
-  
+  let productsData;
+  let colorsData = await Colors.find({});
+  let filterData;
+  let parseFilter;
+  let findElements;
+
   if (category) {
     if (filter) {
-      filter.split(';').forEach(element => {
-        const newElement = element.split('=')
-        parseFilter = { ...parseFilter, [newElement[0]]: newElement[1].split(',') }
-      })
-  
-      const { 
-        colorWithMaterialFilter, 
-        typeFilter, 
-        priceFilter, 
-        widthFilter, 
-        heightFilter, 
-        lengthFilter 
-      } = searchProductsByElementFilter(parseFilter, colorsData)
-    
-      findElements = { 
+      filter.split(";").forEach((element) => {
+        const newElement = element.split("=");
+        parseFilter = {
+          ...parseFilter,
+          [newElement[0]]: newElement[1].split(","),
+        };
+      });
+
+      const {
+        colorWithMaterialFilter,
+        typeFilter,
+        priceFilter,
+        widthFilter,
+        heightFilter,
+        lengthFilter,
+      } = searchProductsByElementFilter(parseFilter, colorsData);
+
+      findElements = {
         $and: [
-          { category: category }, 
-          { type: typeFilter }, 
+          { category: category },
+          { type: typeFilter },
           { colors: colorWithMaterialFilter },
-          { 'quality.standart': priceFilter },
-          { 'characteristics.size.width': widthFilter },
-          { 'characteristics.size.height': heightFilter },
-          { 'characteristics.size.length': lengthFilter }
-        ]
-      }
+          { "quality.standart": priceFilter },
+          { "characteristics.size.width": widthFilter },
+          { "characteristics.size.height": heightFilter },
+          { "characteristics.size.length": lengthFilter },
+        ],
+      };
     } else {
-      findElements = { category: category }
+      findElements = { category: category };
     }
   } else {
-    findElements = {}
+    findElements = {};
   }
 
-  let query = findElements
+  let query = findElements;
   let options = {
     sort: sortProducts(sort),
     page,
-    limit
-  }
+    limit: limit || 10,
+  };
 
-  const allProducts = await Products.paginate(query, { pagination: false })
+  const allProducts = await Products.paginate(query, { pagination: false });
 
-  filterData = createFilterData(allProducts.docs, colorsData, parseFilter)
+  filterData = createFilterData(allProducts.docs, colorsData, parseFilter);
 
-  const products = await Products.paginate(query, options)
+  const products = await Products.paginate(query, options);
 
-  productsData = products.docs
+  productsData = products.docs;
 
-  const totalPages = products.totalPages
-  const currentPage = products.page
-    
-  return { productsData, totalPages, currentPage, filterData }
-})
+  const totalPages = products.totalPages;
+  const currentPage = products.page;
 
-module.exports = paginationProductsList
+  return { productsData, totalPages, currentPage, filterData };
+};
+
+module.exports = paginationProductsList;
