c9c11ca398d1c548cd1d6db88a3d9ffcbeea469b
Ksena <ksena@ip-192-168-1-7.ec2.internal>
2024-03-19 11:06:12 +0100

    new change

--- routes/colors.routes.js
+++ routes/colors.routes.js
@@ -1,25 +1,22 @@
-const { Router } = require('express') 
-const router = Router()
-const colorsModel = require('../models/color')
+const { Router } = require("express");
+const router = Router();
+const colorsModel = require("../models/color");
 
-router.get(
-  '/',
-  async (req, res) => {
-    try {
-      const dataModel = await colorsModel.find()
+router.get("/", async (req, res) => {
+  try {
+    const dataModel = await colorsModel.find();
 
-      let colors = await dataModel.map(item => ({
-        id: item._id,
-        name: item.name.replace("-", " "),
-        img: item.img,
-        material: item.material,
-      }))
+    let colors = await dataModel.map((item) => ({
+      id: item._id,
+      name: item.name.replace("-", " "),
+      img: item.img,
+      material: item.material,
+    }));
 
-      res.json(colors)
-    } catch (e) {
-      res.status(500).json({ message: "its Error, try again!" })
-    }
+    res.json(colors);
+  } catch (e) {
+    res.status(500).json({ message: "its Error, try again!" });
   }
-)
+});
 
-module.exports = router
+module.exports = router;
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,114 +1,106 @@
-const { Router } = require('express') 
-const router = Router()
-const productModel = require('../models/product')
-const colorsModel = require('../models/color')
-const lastItems = require('../utils/lastItems')
-const paginationByElement = require('../utils/paginationByElement')
-const paginationProductsList = require('../utils/paginationProductsList')
-const productsCursor = require('../utils/productsCursor')
-const quantityElementsToDisplay = require('../utils/constants/quantityElementsToDisplay')
+const { Router } = require("express");
+const router = Router();
+const productModel = require("../models/product");
+const colorsModel = require("../models/color");
+const lastItems = require("../utils/lastItems");
+const paginationByElement = require("../utils/paginationByElement");
+const paginationProductsList = require("../utils/paginationProductsList");
+const productsCursor = require("../utils/productsCursor");
+const quantityElementsToDisplay = require("../utils/constants/quantityElementsToDisplay");
 
-router.get(
-  '/new',
-  async (req, res) => {
-    try {
-      const { dataModel } = await lastItems(productModel, quantityElementsToDisplay)
+router.get("/new", async (req, res) => {
+  try {
+    const { dataModel } = await lastItems(
+      productModel,
+      quantityElementsToDisplay
+    );
 
-      const { products } = await productsCursor(dataModel)
+    const { products } = await productsCursor(dataModel);
 
-      res.json(products)
-    } catch (e) {
-      res.status(500).json({ message: "its Error, try again!" })
-    }
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ message: "its Error, try again!" });
   }
-)
+});
 
-router.get(
-  '/list/:category',
-  async (req, res) => {
-    try {
-      const { category } = req.params
-      const { page, limit, filter, sort } = req.query
+router.get("/list/:category", async (req, res) => {
+  try {
+    const { category } = req.params;
+    const { page, limit, filter, sort } = req.query;
 
-      const { productsData, totalPages, currentPage, filterData } = await paginationProductsList({ 
-        Products: productModel, 
-        Colors: colorsModel, 
-        category, 
-        page, 
+    const { productsData, totalPages, currentPage, filterData } =
+      await paginationProductsList({
+        Products: productModel,
+        Colors: colorsModel,
+        category,
+        page,
         limit,
         filter,
-        sort
-      })
+        sort,
+      });
 
-      const { products } = await productsCursor(productsData)
+    const { products } = await productsCursor(productsData);
 
-      res.json({
-        pageData: products,
-        totalPages,
-        currentPage,
-        filterData
-      })
-    } catch (e) {
-      res.status(500).json({ message: "its Error, try again!" })
-    }
+    res.json({
+      pageData: products,
+      totalPages,
+      currentPage,
+      filterData,
+    });
+  } catch (e) {
+    res.status(500).json({ message: "its Error, try again!" });
   }
-)
+});
 
-router.get(
-  '/:element/:elementName',
-  async (req, res) => {
-    try {
-      const { element, elementName } = req.params
-      const { page, limit } = req.query
+router.get("/:element/:elementName", async (req, res) => {
+  try {
+    const { element, elementName } = req.params;
+    const { page, limit } = req.query;
 
-      const { dataModel, totalPages, currentPage } = await paginationByElement({ 
-        Model: productModel, 
-        element,
-        elementName, 
-        page, 
-        limit 
-      })
+    const { dataModel, totalPages, currentPage } = await paginationByElement({
+      Model: productModel,
+      element,
+      elementName,
+      page,
+      limit,
+    });
 
-      const { products } = await productsCursor(dataModel)
+    const { products } = await productsCursor(dataModel);
 
-      res.json({
-        pageData: products,
-        totalPages,
-        currentPage,
-      })
-    } catch (e) {
-      res.status(500).json({ message: "its Error, try again!" })
-    }
+    res.json({
+      pageData: products,
+      totalPages,
+      currentPage,
+    });
+  } catch (e) {
+    res.status(500).json({ message: "its Error, try again!" });
   }
-)
+});
 
-router.get(
-  '/:id',
-  async (req, res) => {
-    try {
-      const { id } = req.params
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
 
-      let findProduct = await productModel.find({ _id: id })
+    let findProduct = await productModel.find({ _id: id });
 
-      let product = await findProduct.map(item => ({
-        id: item._id,
-        name: item.name,
-        code: item.code,
-        category: item.category,
-        type: item.type,
-        popularity: item.popularity,
-        quality: item.quality,
-        colors: item.colors,
-        characteristics: item.characteristics,
-        imgs: item.imgs,
-        about: item.about
-      }))
+    let product = await findProduct.map((item) => ({
+      id: item._id,
+      name: item.name,
+      code: item.code,
+      category: item.category,
+      type: item.type,
+      popularity: item.popularity,
+      quality: item.quality,
+      colors: item.colors,
+      characteristics: item.characteristics,
+      imgs: item.imgs,
+      about: item.about,
+    }));
 
-      res.json(product[0])
-    } catch (e) {
-      res.status(500).json({ message: "its Error, try again!" })
-    }
+    res.json(product[0]);
+  } catch (e) {
+    res.status(500).json({ message: "its Error, try again!" });
   }
-)
+});
 
-module.exports = router
+module.exports = router;
